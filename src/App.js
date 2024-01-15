a40acf5acac747d35f85d189a00a7d99640d1db4
Jefferson Alves <100549763+alvesjeff05@users.noreply.github.com>
2024-01-15 17:19:33 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Quizz from './quizz';
+import './quizz/style.css'
+
+function App() {
+  return (
+    <>
+    <span id="title">Quiz: One Piece</span>
+    <Quizz></Quizz>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Data/perguntas.js
@@ -0,0 +1,98 @@
+export const Perguntas  = [
+    {    
+    pergunta:'Qual é o nome do protagonista de One Piece?',
+        opcoesResposta:[
+            {resposta:'Monkey D. Luffy', correta:true, alternativa:'A)'},
+            {resposta:'Roronoa Zoro', correta:false, alternativa:'B)'},
+            {resposta:'Nami', correta:false, alternativa:'C)'},
+            {resposta:'Usopp', correta:false, alternativa:'D)'},
+        ]
+    },
+
+    {
+    pergunta: 'Quem é o capitão dos Piratas do Chapéu de Palha?',
+    opcoesResposta:[
+        {resposta:'Roronoa Zoro', correta:false, alternativa:'A)'},
+        {resposta:'Monkey D. Luffy', correta:true, alternativa:'B)'},
+        {resposta:'Nami', correta:false, alternativa:'C)'},
+        {resposta:'Usopp', correta:false, alternativa:'D)'},
+    ]
+},
+
+    {
+    pergunta: 'Qual é o nome do navio principal dos Piratas do Chapéu de Palha?',
+    opcoesResposta:[
+        {resposta:'Going Merry', correta:false, alternativa:'A)'},
+        {resposta:'Thousand Sunny', correta:true, alternativa:'B)'},
+        {resposta:'Red Force', correta:false, alternativa:'C)'},
+        {resposta:'Victory Hunter', correta:false, alternativa:'D)'},
+    ]
+},
+    
+    {
+    pergunta: 'Quem é considerado o espadachim mais habilidoso dos Piratas do Chapéu de Palha?',
+    opcoesResposta:[
+        {resposta:'Sanji', correta:false, alternativa:'A)'},
+        {resposta:'Nico Robin', correta:false, alternativa:'B)'},
+        {resposta:'Roronoa Zoro', correta:true, alternativa:'C)'},
+        {resposta:'Franky', correta:false, alternativa:'D)'},
+    ]
+},
+
+
+    {
+    pergunta: 'Quem é o imperador que controla a Ilha dos Tritões?',
+    opcoesResposta:[
+        {resposta:'Kaido', correta:false, alternativa:'A)'},
+        {resposta:'Big Mom', correta:true, alternativa:'B)'},
+        {resposta:'Shanks', correta:false, alternativa:'C)'},
+        {resposta:'Blackbeard', correta:false, alternativa:'D)'},
+    ]},
+
+    {
+    pergunta: 'Qual é o nome do navio de Eustass Kid?',
+    opcoesResposta:[
+        {resposta:'Bezan Black', correta:false, alternativa:'A)'},
+        {resposta:'Queen Mama Chanter', correta:false, alternativa:'B)'},
+        {resposta:'Rocinante', correta:false, alternativa:'C)'},
+        {resposta:'Victoria Punk', correta:true, alternativa:'D)'},
+    ]},
+
+
+    {
+    pergunta: 'Quem é o arqueólogo dos Piratas do Chapéu de Palha?',
+    opcoesResposta:[
+        {resposta:'Sanji', correta:false, alternativa:'A)'},
+        {resposta:'Nami', correta:false, alternativa:'B)'},
+        {resposta:'Nico Robin', correta:true, alternativa:'C)'},
+        {resposta:'Brook', correta:false, alternativa:'D)'},
+    ]},
+
+    {
+    pergunta: 'Qual é a habilidade da Fruta Hie Hie no Mi, consumida por Almirante Aokiji?',
+    opcoesResposta:[
+        {resposta:'Manipulação de fogo', correta:false, alternativa:'A)'},
+        {resposta:'Manipulação de gelo', correta:true, alternativa:'B)'},
+        {resposta:'Manipulação de pedra', correta:false, alternativa:'C)'},
+        {resposta:'Manipulação de água', correta:false, alternativa:'D)'},
+    ]},
+
+
+    {
+    pergunta: 'Quem é o criador do Exército Revolucionário em One Piece?',
+    opcoesResposta:[
+        {resposta:'Monkey D. Dragon', correta:true, alternativa:'A)'},
+        {resposta:'Gol D. Roger', correta:false, alternativa:'B)'},
+        {resposta:'Sabo', correta:false, alternativa:'C)'},
+        {resposta:'Nico Robin', correta:false, alternativa:'D)'},
+    ]},
+
+    {
+    pergunta: 'Qual é o nome do país governado por Kaido, um dos Yonkou?',
+    opcoesResposta:[
+        {resposta:'Wano', correta:true, alternativa:'A)'},
+        {resposta:'Elbaf', correta:false, alternativa:'B)'},
+        {resposta:'Zou', correta:false, alternativa:'C)'},
+        {resposta:'Skypiea', correta:false, alternativa:'D)'},
+    ]},
+]
